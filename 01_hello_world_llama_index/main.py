ddeb6ec1b04ad4341fd9ca2210d3cba75b5b40b2
amnotme <leopoldo.hernandez.oliva@gmail.com>
2024-02-12 13:20:28 -0600

    adding pine vector store and uploading to documents to i

--- 01_hello_world_llama_index/main.py
+++ 01_hello_world_llama_index/main.py
@@ -5,7 +5,9 @@ import llama_index as llama  # Import the llama_index library
 
 def main(url: str) -> None:
     # Read web page content and convert it into text
-    documents = llama.readers.SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
+    documents = llama.readers.SimpleWebPageReader(html_to_text=True).load_data(
+        urls=[url]
+    )
 
     # Create an index from the documents
     index = llama.VectorStoreIndex.from_documents(documents=documents)
@@ -14,13 +16,13 @@ def main(url: str) -> None:
     query_engine = index.as_query_engine()
 
     # Execute a query on the index
-    response = query_engine.query('What is LlamaIndex')
+    response = query_engine.query("What is LlamaIndex")
 
     # Print the response
     print(response)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load environment variables from .env file
     load_dotenv()
 
@@ -30,4 +32,4 @@ if __name__ == '__main__':
         raise ValueError("OPENAI_API_KEY environment variable is not set.")
 
     # Call the main function with the specified URL
-    main(url='https://www.llamaindex.ai/')
+    main(url="https://www.llamaindex.ai/")
--- 02_documentation_helper_llamaindex/ingestion.py
+++ 02_documentation_helper_llamaindex/ingestion.py
@@ -5,7 +5,6 @@ from llama_index.node_parser import SimpleNodeParser
 from llama_index.llms import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index import (
-    download_loader,
     ServiceContext,
     VectorStoreIndex,
     StorageContext,
@@ -22,9 +21,28 @@ if __name__ == "__main__":
     pc = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
     loader = UnstructuredReader()
     directory_reader = SimpleDirectoryReader(
-        input_dir="./llamaindex_docs_tmp", file_extractor={".html": UnstructuredReader()}
+        input_dir="./llamaindex_docs",
+        file_extractor={".html": UnstructuredReader()},
     )
-    documents = directory_reader.load_data()
+    documents = directory_reader.load_data(show_progress=True)
     node_parser = SimpleNodeParser.from_defaults(chunk_size=500, chunk_overlap=20)
-    nodes = node_parser.get_nodes_from_documents(documents=documents)
-    print("Going to investigation")
+    llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
+    embed_model = OpenAIEmbedding(
+        model_name="text-embedding-ada-002", embed_batch_size=100
+    )
+    service_context = ServiceContext.from_defaults(
+        llm=llm, embed_model=embed_model, node_parser=node_parser
+    )
+
+    index_name = "llamaindex-documentation-helper"
+    pinecone_index = pc.Index(name=index_name, host=os.getenv("PINECONE_INDEX_HOST"))
+    vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex.from_documents(
+        documents=documents,
+        storage_context=storage_context,
+        service_context=service_context,
+        show_progress=True,
+    )
+    print("Ingestion finalized")
