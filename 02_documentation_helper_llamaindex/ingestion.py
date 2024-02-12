8064f37be25975119448bc17c667edecce23533b
amnotme <leopoldo.hernandez.oliva@gmail.com>
2024-02-12 12:44:55 -0600

    Moving the Pipfile to root and updating the ingestion.py file

--- 02_documentation_helper_llamaindex/ingestion.py
+++ 02_documentation_helper_llamaindex/ingestion.py
@@ -1,8 +1,30 @@
 from dotenv import load_dotenv
 import os
+from llama_index import SimpleDirectoryReader
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.llms import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index import (
+    download_loader,
+    ServiceContext,
+    VectorStoreIndex,
+    StorageContext,
+)
+from llama_index.vector_stores import PineconeVectorStore
+from llama_hub.file.unstructured import UnstructuredReader
+import pinecone
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     load_dotenv()
-    if not os.getenv('OPENAI_API_KEY') or not os.getenv('PINECONE_API_KEY'):
-        raise Exception('Not all environment variables have been loaded')
-    print('Going to investigation')
+    if not os.getenv("OPENAI_API_KEY") or not os.getenv("PINECONE_API_KEY"):
+        raise Exception("Not all environment variables have been loaded")
+    pc = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+    loader = UnstructuredReader()
+    directory_reader = SimpleDirectoryReader(
+        input_dir="./llamaindex_docs_tmp", file_extractor={".html": UnstructuredReader()}
+    )
+    documents = directory_reader.load_data()
+    node_parser = SimpleNodeParser.from_defaults(chunk_size=500, chunk_overlap=20)
+    nodes = node_parser.get_nodes_from_documents(documents=documents)
+    print("Going to investigation")
