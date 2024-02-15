7b3affe246719c53a25842d8e0ac855893b061f9
amnotme <leopoldo.hernandez.oliva@gmail.com>
2024-02-15 07:43:35 -0600

    adding punkt

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import nltk
 nltk_data_dir = "./resources/nltk_data_dir/"
 if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir, exist_ok=True)
-
+nltk.data.path.clear()
 nltk.data.path.append(nltk_data_dir)
 nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
 
 import openai
 from time import sleep
